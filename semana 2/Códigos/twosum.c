b4f50a8501a7dd76cc7e2905f38899ceff44e620
Eduarda Medeiros <165853048+duhdah@users.noreply.github.com>
2024-04-28 20:31:15 -0300

    Leet Code

--- semana 2/Códigos/twosum.c
+++ semana 2/Códigos/twosum.c
@@ -1,18 +1,17 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *ret;
     for(int i = 0; i < numsSize; i++){
         for(int j = i + 1; j < numsSize; j++){
             if((*(nums+i) + *(nums+j)) == target){
-                int *ret,*p;
-                ret = (*int)malloc(2*sizeof(int));
-                *ret = i;
-                p = ret;
-                p++;
-                *p = j;
+                int *p;
+                ret = (int*)malloc(2*sizeof(int));
+                ret[0] = i;
+                ret[1] = j;
+                *returnSize = 2;
                 return ret;
             }
         }
     }
+    *returnSize = 0;
+    return NULL;
 }
