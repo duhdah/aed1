a5731b7c909c6cc97df5d37ff08abf8cce1ae873
Eduarda Medeiros <165853048+duhdah@users.noreply.github.com>
2024-06-23 20:49:10 -0300

    colocando os códigos na pasta dos LeetCodes

--- semana 8/LeetCodes da semana/reverseVowels.c
+++ semana 8/LeetCodes da semana/reverseVowels.c
@@ -1,4 +1,5 @@
 /*
+
 345. Reverse Vowels of a String
 
 Given a string s, reverse only all the vowels in the string and return it.
@@ -6,6 +7,7 @@ Given a string s, reverse only all the vowels in the string and return it.
 The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
 Link: https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+
 */
 
 // Funcionou (6 ms)
